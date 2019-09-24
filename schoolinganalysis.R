e7941566894071e3a726005152c4c28195f36fe4
Peter Korosi <korosipe@gmail.com>
2019-09-24 12:46:53 +0200

    point plots, boxplot

--- schoolinganalysis.R
+++ schoolinganalysis.R
@@ -5,8 +5,11 @@ summary(schooling)
 
 # iskolázottság és munkatapasztalat összefüggése:
 ggplot(schooling, aes(ED, EXP)) + geom_point()
+
 # iskolázottság és logaritmált bér összefüggése:
 ggplot(schooling, aes(ED, LWAGE)) + geom_point()
+#elég jól látszik, hogy a nagyobb iskolázottság növeli a jövőbeli bért
+
 # a logaritmált bérből vegyük ki az outliereket:
 # (igazából ezek is fontos adatok, de nem tudtam jobbat kitalálni)
 clearIrrelevantLWage <- function(schooling) {
@@ -15,3 +18,15 @@ clearIrrelevantLWage <- function(schooling) {
 clearIrrelevantLWage(schooling)
 ggplot(schooling, aes(ED, LWAGE)) + geom_point()
 # így kicsit tisztult a kép
+
+longdata <- melt(schooling, measure.vars = c("EXP", "LWAGE", "BLK"))
+ggplot(longdata, aes(value)) + geom_histogram(bins = 10) + 
+  facet_wrap(~ variable, scales = "free_x")
+
+#logaritmált bér a munkatapasztalat függvényében,
+# a baloldali ábrán a fehér, a jobb oldalin a fekete munkások bére:
+ggplot(schooling, aes(EXP,LWAGE)) + geom_point() + facet_wrap(~ as.factor(BLK))
+
+# logaritmált bér az iskolázottság függvényében, baloldalon északi államok lakói,
+# jobb oldalon déli államok lakói:
+ggplot(schooling, aes(ED,LWAGE)) + geom_boxplot() + facet_wrap(~ as.factor(SOUTH))
